71499c281af2f0d3ddab26efcb7184adc92947f0
balajisivakumar <text2balaji@gmail.com>
2026-04-07 22:35:50 +0100

    April-7

---
+++ reverse_number_pattern.py
@@ -0,0 +1,17 @@
+n = 5
+
+for i in range(5):
+    row = ""
+    for j in range(n-i):
+        row = row + str(j+1)
+    print(row)
+
+
+
+for i in range(5):
+    row = ""
+    for j in range(n-i):
+        row = row + str(j+1)
+    print(row)
+
+
